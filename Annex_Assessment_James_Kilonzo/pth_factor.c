b47d2abdc626e9455449d121d0bfc618067288fa
James Kilonzo <jkilonzo2023@gmail.com>
2026-05-08 12:02:18 +0300

    Comment correction

--- Annex_Assessment_James_Kilonzo/pth_factor.c
+++ Annex_Assessment_James_Kilonzo/pth_factor.c
@@ -9,6 +9,8 @@ long pthFactor(long n, long p) {
     long small_factors[100000]; // Approximately O(sqrt[N])
     long count = 0;
 
+
+    // Find the small factors -- first half of the O(sqrt[N])
     for (long d = 1; d * d <= n; d++) {
         if (n % d == 0) {
             small_factors[count] = d;
@@ -16,16 +18,25 @@ long pthFactor(long n, long p) {
         }
     }
 
+
+    // When p is already in the first half (small factors)
     if (p < count) {
         return small_factors[p-1];
     }
 
+
+    // Working out the large factors - second half of the O(sqrt[N])
+
     p = p - count;
     long last_index = count - 1;
 
+
+    // Edge case when dealing with Perfect Squares - Avoid duplicates
     if (small_factors[last_index] * small_factors[last_index] == n) {
         last_index = last_index - 1;
     }
+
+    // Returning the large factors in order - correctly 
 
     if (p <= last_index +1) {
         return n / small_factors[last_index - (p-1)];
